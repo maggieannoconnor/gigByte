b36d0a45a95dec893fafc5d29d8c9b658832dbe4
Robbie Boyle <rvb8xv@virginia.edu>
2023-11-26 21:53:25 -0500

    added basic routing

---
+++ account.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <meta charset="UTF-8">  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Maggie O'Connor and Robbie Boyle">
+  <meta name="description" content="index page for gigByte">  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+</head>
+<body>
+<?php include("header.html");?>
+<header class="jumbotron text-center">
+    <p class="lead">Account</p>
+</header>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?php include("footer.html");?>   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <meta charset="UTF-8">  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Maggie O'Connor and Robbie Boyle">
+  <meta name="description" content="index page for gigByte">  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+</head>
+
+<body>
+<?php include("header.html");?>
+
+<div class="container"> 
+<header class="jumbotron text-center">
+    <h1 class="display-4">Welcome to GigByte</h1>
+    <p class="lead">Discover bands and upcoming events.</p>
+</header>
+
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<?php include("footer.html");?>   
+</body>
+</html>
