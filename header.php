820332d4fa5f5d126aa60264aa48cea5095a08bc
maggieannoconnor <mao3mq@virginia.edu>
2023-12-03 20:18:22 -0500

    Header, Footer, and Venue html

--- header.php
+++ header.php
@@ -13,18 +13,20 @@ if (isset($_POST["signout"])) {
     exit();
 }
 ?>
-
-<header>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">GigByte</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"
-                aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav ms-auto">
-                    <?php
+<header class="site-header sticky-top" style="margin-bottom: 10px"> 
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #232D4B;">
+      <div class="container-fluid">            
+        <a class="navbar-brand p-1" href="index.php">
+          <!-- <img src="https://i1.wp.com/ajaylimaye.com/wp-content/uploads/2019/06/1024px-University_of_Virginia_Rotunda_logo.svg_.png?fit=686%2C700&ssl=1" alt="UVA LOGO" width="30" height="30">
+          -->
+          GigByte
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ms-auto">
+              <?php
                     if (isset($_SESSION["id"])) {
                         // User is logged in, all links
                         echo '<li class="nav-item dropdown">
@@ -52,8 +54,9 @@ if (isset($_POST["signout"])) {
                         echo '<li class="nav-item d-none d-md-block"><a class="nav-link" href="login.php">Login</a></li>';
                     }
                     ?>
-                </ul>
-            </div>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-</header>
+  </header> 
--- venues.php
+++ venues.php
@@ -1,119 +1,112 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 require("connect-db.php");
 require("gigbyte-db.php");
 
 // To list out all venues
 $list_of_venues = getAllVenues();
-if  ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  if (!empty($_POST['updateBtn']))
-  {
-    echo $_POST['venuename_to_update'];
-  }
-  else if (!empty($_POST['confirmUpdateBtn'])){
-    updateVenue($_POST['venue_id'], $_POST['name'], $_POST['address']);
-    $list_of_venues = getAllVenues();
-  }
-  else if (!empty($_POST['deleteBtn'])){
-    deleteVenue($_POST['venueid_to_delete']);
-    $list_of_venues = getAllVenues();
-  }
-  else if (!empty($_POST['actionBtn']))
-  {
-    addVenue($_POST['name'], $_POST['address']);
-    $list_of_venues = getAllVenues(); // name, address
-  }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['updateBtn'])) {
+        echo $_POST['venuename_to_update'];
+    } else if (!empty($_POST['confirmUpdateBtn'])) {
+        updateVenue($_POST['venue_id'], $_POST['name'], $_POST['address']);
+        $list_of_venues = getAllVenues();
+    } else if (!empty($_POST['deleteBtn'])) {
+        deleteVenue($_POST['venueid_to_delete']);
+        $list_of_venues = getAllVenues();
+    } else if (!empty($_POST['actionBtn'])) {
+        addVenue($_POST['name'], $_POST['address']);
+        $list_of_venues = getAllVenues(); // name, address
+    }
 }
 ?>
 
-<!DOCTYPE html> 
-<html> 
-<head> 
-  <meta charset="UTF-8">  
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Maggie O'Connor">
-  <meta name="description" content="List of Venues for GigByte">  
-  <title>GigByte</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <!--icon displayed in browzer tab, important for usability!!-->
-  <!-- If you choose to use a favicon, specify the destination of the resource in href -->
-  <!-- <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" /> -->
-</head>
-<body>
-  <?php include("header.php");?>
-  <h3>Add Venue</h3>  
-  <form name="mainForm" action="venues.php" method="post"> 
-    <input type="hidden" class="form-control" name="venue_id" required 
-          value="<?php echo $_POST['venueid_to_update'];?>"
-    />   
-    <div class="row mb-3 mx-3">
-      Venue Name:
-      <input type="text" class="form-control" name="name" required 
-          value="<?php echo $_POST['venuename_to_update'];?>"
-      /> 
-    </div>
-    <div class="row mb-3 mx-3">
-      Address:
-      <input type="text" class="form-control" name="address" required 
-          value="<?php echo $_POST['venueaddress_to_update'];?>"
-      />        
-    </div>   
-    <div class="row mb-3 mx-3">
-      <input type="submit" value="Add Venue" name="actionBtn" 
-              class="btn btn-primary" title="Insert a venue into venue" required />   
-    </div>  
-    <div class="row mb-3 mx-3">
-      <input type="submit" value="Confirm Update" name="confirmUpdateBtn" 
-              class="btn btn-secondary" title="Update a venues's information" required />   
-    </div>  
-  </form> 
-  <hr/>
-  <h3>Venues</h3>
-  <div class="row justify-content-center">  
-  <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
-    <thead>
-    <tr style="background-color:#B0B0B0">
-      <th width="30%">Name        
-      <th width="60%">Address       
-      <th>&nbsp;</th>
-      <th>&nbsp;</th>
-    </tr>
-    </thead>
+<!DOCTYPE html>
+<html>
 
-  <?php foreach ($list_of_venues as $venue): ?> 
-    <tr>  <!-- each row --> 
-      <td><?php echo $venue['name']; ?></td> <!-- each column --> 
-      <td><?php echo $venue['address']; ?></td>        
-      <td> 
-          <form action="venues.php" method="post">
-            <input type="submit" value="Update" name="updateBtn" class="btn btn-secondary" />
-            <!--pass in all information that could be updated-->
-            <input type="hidden" name="venueid_to_update" 
-                value="<?php echo $venue['venue_id']; ?>"
-            />
-            <input type="hidden" name="venuename_to_update" 
-                value="<?php echo $venue['name']; ?>"
-            />
-            <input type="hidden" name="venueaddress_to_update" 
-                value="<?php echo $venue['address']; ?>"
-            />
-          </form>
-        </td>   
-        <td>
-          <form action="venues.php" method="post">
-            <input type="submit" value="Delete" name="deleteBtn" class="btn btn-danger"  />
-            <input type="hidden" name="venueid_to_delete" 
-                value="<?php echo $venue['venue_id']; ?>"
-            />
-          </form>  
-        </td>   
-    </tr>
-  <?php endforeach; ?>
-  </table>
-  </div>  
-  <?php include("footer.html");?>   
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Maggie O'Connor">
+    <meta name="description" content="List of Venues for GigByte">
+    <title>GigByte</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--icon displayed in browzer tab, important for usability!!-->
+    <!-- If you choose to use a favicon, specify the destination of the resource in href -->
+    <!-- <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" /> -->
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <div class="mx-4">
+        <h3>Venues</h3>
+        <?php foreach ($list_of_venues as $venue): ?>
+        <div class="card d-flex mb-3">
+            <div class="card-header d-flex justify-content-between align-items-center"
+                style="background-color: #eef0f7 !important;" id="student_info_header">
+                <div class="me-auto p-2">
+                    <h5 class="mb-0">
+                        <?php echo $venue['name']; ?>
+                    </h5>
+                </div>
+
+                <div class="p-2">
+                    <form action="venues.php" method="post">
+                        <input type="submit" value="Update" name="updateBtn" class="btn btn-secondary mr-2" />
+                        <!--pass in all information that could be updated-->
+                        <input type="hidden" name="venueid_to_update"
+                            value="<?php echo $venue['venue_id']; ?>" />
+                        <input type="hidden" name="venuename_to_update"
+                            value="<?php echo $venue['name']; ?>" />
+                        <input type="hidden" name="venueaddress_to_update"
+                            value="<?php echo $venue['address']; ?>" />
+                    </form>
+                </div>
+
+                <div class="p-2">
+                    <form action="venues.php" method="post">
+                        <input type="submit" value="Delete" name="deleteBtn" class="btn btn-danger" />
+                        <input type="hidden" name="venueid_to_delete"
+                            value="<?php echo $venue['venue_id']; ?>" />
+                    </form>
+                </div>
+
+            </div>
+            <div class="card-body">
+              <p style="margin-right: 10px;" class="mb-0"><b>Address: </b><?php echo $venue['address']; ?></p>
+            </div>
+        </div>
+        <?php endforeach; ?>
+
+    <h3>Add Venue</h3>
+    <form name="mainForm" action="venues.php" method="post">
+        <input type="hidden" class="form-control" name="venue_id" required
+            value="<?php echo $_POST['venueid_to_update']; ?>" />
+        <div class="row mb-3 mx-3">
+            Venue Name:
+            <input type="text" class="form-control" name="name" required
+                value="<?php echo $_POST['venuename_to_update']; ?>" />
+        </div>
+        <div class="row mb-3 mx-3">
+            Address:
+            <input type="text" class="form-control" name="address" required
+                value="<?php echo $_POST['venueaddress_to_update']; ?>" />
+        </div>
+        <div class="row mb-3 mx-3">
+            <input type="submit" value="Add Venue" name="actionBtn" class="btn btn-primary"
+                title="Insert a venue into venue" required />
+        </div>
+        <div class="row mb-3 mx-3">
+            <input type="submit" value="Confirm Update" name="confirmUpdateBtn" class="btn btn-secondary"
+                title="Update a venues's information" required />
+        </div>
+    </form>
+    </div>
+    </div>
+    <?php include("footer.html"); ?>
 </body>
+
 </html>
